6c60fed97f4103e381a5c6c1a947a59e0210cc88
BFR.DnB <slavix.dnb@gmail.com>
2019-04-19 16:26:40 +0300

    Volume change on longpress/touchstart

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ Taskkill /IM GoRemote-i386.exe /F
 del /f GoRemote-i386.exe
 go generate
 go build -ldflags -H="windowsgui -s -w" -o "GoRemote-i386.exe"
-rice append /exec "GoRemote-i386.exe"
+rice append --exec "GoRemote-i386.exe"
 
 set GOOS=windows
 set GOARCH=amd64
@@ -12,5 +12,5 @@ Taskkill /IM GoRemote-amd64.exe /F
 del /f GoRemote-amd64.exe
 go generate
 go build -ldflags -H="windowsgui -s -w" -o "GoRemote-amd64.exe"
-rice append /exec "GoRemote-amd64.exe"
+rice append --exec "GoRemote-amd64.exe"
 
